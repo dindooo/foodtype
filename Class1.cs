fb8febcc503bc1ff5b705dea0657c9b9c8e8b3d9
dinooo <145918377+dindooo@users.noreply.github.com>
2023-12-12 11:13:35 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodType
+{
+    internal class foodType
+    {
+        string name;
+        int protein;
+        int carbs;
+        int fat;
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public void setProtein(int protein)
+        {
+            this.protein = protein;
+        }
+        public int getProtein()
+        {
+            return this.protein;
+        }
+        public void setCarbs(int carbs)
+        {
+            this.carbs = carbs; 
+        }
+        public int getCarbs()
+        {
+            return this.carbs;
+        }
+        public void setFat(int fat)
+        {
+            this.fat = fat; 
+        }
+        public int getFat()
+        {
+            return this.fat;
+        }
+        public foodType(string name, int protein, int carbs, int fat)
+        {
+            this.name = name;
+            this.protein = protein;
+            this.carbs = carbs;
+            this.fat = fat;
+        }
+        public override string ToString()
+        {
+            string ispis = "Naziv " + this.getName()
+            + " Proteini " + this.getProtein()
+            + " Carbs " + this.getCarbs()
+            + " Fat " + this.getFat();
+            return ispis;
+        }
+
+        public foodType()
+        {
+
+        }
+
+       
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foodType foodtype1 = new foodType();
+
+            foodtype1.setName("Banana");
+            foodtype1.setProtein(4);
+            foodtype1.setCarbs(93);
+            foodtype1.setFat(3);
+
+            Console.WriteLine(foodtype1.ToString());
+
+            foodType foodtype2 = new foodType("Jagoda", 2, 67, 4);
+
+            Console.WriteLine(foodtype2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
